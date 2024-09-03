79579cde2f09b399f785bcae57e2424ff41fdeec
badreddine <153949545+BdAbiy@users.noreply.github.com>
2024-09-03 15:16:25 +0100

    Add files via upload
    upload

---
+++ lib/server.java
@@ -0,0 +1,13 @@
+/**
+ * simple Tcp server
+ * handles connections
+ */
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class server {
+    public server(){
+        
+    }
+    
+}
